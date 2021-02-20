4deecef0683af6345ab63656ddeda7df19a3ecb2
Bazan Milla <victor_bazan_milla@optum.com>
2021-02-20 22:41:56 +0100

    Get category from useParams

--- src/Products.jsx
+++ src/Products.jsx
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 
 import useFetch from './useFetch';
 import Spinner from './Spinner';
+import { useParams } from "react-router-dom";
 
 export default function Products() {
 
   const [size, setSize] = useState("");
+  const { category } = useParams();
+
   const { data: products, error, loading } = useFetch(
-    "products?category=shoes"
+    `products?category=${category}`
   )
 
   function renderProduct(p) {
