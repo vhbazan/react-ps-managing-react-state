075ae580d8e9f19328fb1b537652506f19e02806
Bazan Milla <victor_bazan_milla@optum.com>
2021-02-20 20:47:57 +0100

    Use useFecth in our App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import { getProducts } from './services/productService';
 import Spinner from './Spinner';
+import useFetch from './useFetch';
 
 export default function App() {
 
   const [size, setSize] = useState("");
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function init() {
-      try {
-        const response = await getProducts("shoes");
-        setProducts(response);
-      } catch (e) {
-        setError(e)
-      } finally {
-        setLoading(false);
-      };
-    }
-    init();
-  }, []);
+  const { data: products, loading, error } = useFetch("products?category=shoes");
 
   function renderProduct(p) {
     return (
