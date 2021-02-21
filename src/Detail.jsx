99d115c4afe2cd343d8156d842d671d7f8962755
Bazan Milla <victor_bazan_milla@optum.com>
2021-02-21 18:05:56 +0100

    Add select Size dropdown on detail page

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams, useNavigate } from 'react-router-dom';
 import useFetch from './useFetch';
 import Spinner from './Spinner';
@@ -9,7 +9,7 @@ export default function Detail() {
   const { id } = useParams();
   const navigate = useNavigate();
   const { data: product, error, loading } = useFetch("products/" + id);
-
+  const [sku, setSku] = useState("");
   if (loading) return <Spinner />
   if (!product) return <PageNotFound />
   if (error) throw error;
@@ -18,6 +18,13 @@ export default function Detail() {
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p id="price">${product.price}</p>
+      <select id="size" onChange={(e) => setSku(e.target.value)}
+        value={sku}>
+        <option value="">What size?</option>
+        {product.skus.map(sku => (
+          <option value={sku.sku} key={sku.sku}>{sku.size}</option>
+        ))}
+      </select>
       <p>
         <button className="btn btn-primary"
           onClick={() => { navigate("/cart") }}>Add to Cart</button>
