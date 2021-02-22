cf7a5273ecf7a4213373cb568c83399c348aa11e
Bazan Milla <victor_bazan_milla@optum.com>
2021-02-22 22:29:48 +0100

    Display total items in cart's header

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -41,10 +41,14 @@ export default function Cart({ cart, updateQuantity }) {
 
   if (loading) return <Spinner />;
   if (error) throw error;
-
+  const totalItemsInCart = cart.reduce((acc, curr) => acc + curr.quantity, 0)
   return (
     <section id="cart">
-      <h1>Cart</h1>
+      <h1>
+        {
+          totalItemsInCart > 0 ?
+            `${totalItemsInCart} item${totalItemsInCart > 0 ? "s" : ""} in Cart` :
+            "No items in cart"} </h1>
       <ul>{cart.map(renderItem)}</ul>
     </section>
   );
