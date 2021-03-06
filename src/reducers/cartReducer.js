e33977876004016f4e6367d1f29f81ab39cde55e
Bazan Milla <victor_bazan_milla@optum.com>
2021-03-06 12:28:41 +0100

    Add ADD reducer action

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -2,7 +2,17 @@ export default function cartReducer(cart, action) {
   switch (action.type) {
     case 'EMPTY_CART':
       return [];
+    case 'ADD':
+      const { id, sku } = action;
+      const itemInCart = cart.find((item) => item.sku === sku);
+      if (itemInCart) {
+        return cart.map(item => item.id === id ? { ...item, quantity: item.quantity + 1 } : item)
+      }
+      return [...cart,
+      { id, sku, quantity: 1 }
+      ];
+
     default:
-      return cart;
+      throw new Error("Unhandled action")
   }
 }
