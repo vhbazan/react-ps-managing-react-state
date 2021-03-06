9972ac9343c0da06776b7edfba84610ad8f0a397
Bazan Milla <victor_bazan_milla@optum.com>
2021-03-06 13:24:34 +0100

    Create CartProvider component

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -1,3 +1,31 @@
 import React from 'react'
+import { useReducer } from 'react';
+import cartReducer from '../reducers/cartReducer';
+import { useEffect } from 'react';
 
 export const CartContext = React.createContext(null);
+
+let initialCart;
+try {
+  initialCart = JSON.parse(localStorage.getItem('cart')) ?? [];
+} catch {
+  console.error('The cart could not be parsed into JSON');
+  initialCart = [];
+}
+
+export function CartProvider(props) {
+  const [cart, dispatch] = useReducer(cartReducer, initialCart
+  );
+  useEffect(() => {
+    return localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart])
+
+  const contextValue = {
+    cart,
+    dispatch
+  }
+
+  return <CartContext.Provider value={contextValue}>
+    {props.children}
+  </CartContext.Provider>
+}
