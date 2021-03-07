9e58e68870d929043792d612deebc202b0ab9fcc
Bazan Milla <victor_bazan_milla@optum.com>
2021-03-07 12:12:37 +0100

    Start refactoring Checkout.class (use props.dispatch, create state object and remove function, add render method

--- src/Checkout.class.jsx
+++ src/Checkout.class.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { saveShippingAddress } from "./services/shippingService";
-import { useCart } from "./context/cartContext";
 
 const STATUS = {
   IDLE: "IDLE",
@@ -15,20 +14,21 @@ const emptyAddress = {
 };
 
 
-export default function Checkout() {
+export default class Checkout extends React.Component() {
 
-  const { dispatch } = useCart();
+  state = {
+    address: emptyAddress,
+    status: STATUS.IDLE,
+    saveError: null,
+    touched: {}
+  }
 
-  const [address, setAddress] = useState(emptyAddress);
-  const [status, setStatus] = useState(STATUS.IDLE);
-  const [saveError, setSaveError] = useState(null);
-  const [touched, setTouched] = useState({});
+  isValid() {
+    errors = getErrors(this.state.address);
+    return Object.keys(errors).length === 0;
+  }
 
-  // Derived state
-  const errors = getErrors(address);
-  const isValid = Object.keys(errors).length === 0;
-
-  function handleChange(e) {
+  handleChange(e) {
     e.persist(); // persist the event 
     setAddress((currAddress) => {
       return {
@@ -38,7 +38,7 @@ export default function Checkout() {
     })
   }
 
-  function handleBlur(event) {
+  handleBlur(event) {
     event.persist();
     setTouched((currTouched) => {
       return {
@@ -48,13 +48,13 @@ export default function Checkout() {
     })
   }
 
-  async function handleSubmit(event) {
+  async handleSubmit(event) {
     event.preventDefault();
     setStatus(STATUS.SUBMITTING);
     if (isValid) {
       try {
         await saveShippingAddress(address);
-        dispatch({ type: 'EMPTY_CART' });
+        this.props.dispatch({ type: 'EMPTY_CART' });
         setStatus(STATUS.COMPLETED);
       } catch (e) {
         setSaveError(e);
@@ -65,76 +65,80 @@ export default function Checkout() {
 
   }
 
-  function getErrors(address) {
+  getErrors(address) {
     const result = {};
 
     if (!address.city) result.city = "City is required";
     if (!address.country) result.country = "Country is required";
     return result;
   }
+  render() {
 
-  if (saveError) throw saveError;
-  if (status === STATUS.COMPLETED) {
-    return <h1>Thanks for shopping!</h1>
+    if (saveError) { throw saveError; }
+    if (status === STATUS.COMPLETED) {
+      return <h1>Thanks for shopping!</h1>
+    }
+
+    return (
+      <>
+        <h1>Shipping Info</h1>
+        {!isValid && status === STATUS.SUBMITTED && (
+          <div role="alert">
+            <p>Please Fix the following errors</p>
+            <ul>
+              {Object.keys(errors).map((key) => {
+                return <li key={key}>{errors[key]}</li>;
+              })}
+            </ul>
+          </div>
+        )}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="city">City</label>
+            <br />
+            <input
+              id="city"
+              type="text"
+              value={address.city}
+              onBlur={handleBlur}
+              onChange={handleChange}
+            />
+            <p role="alert">
+              {(touched.city || status === STATUS.SUBMITTED) && errors.city}
+            </p>
+          </div>
+
+          <div>
+            <label htmlFor="country">Country</label>
+            <br />
+            <select
+              id="country"
+              value={address.country}
+              onBlur={handleBlur}
+              onChange={handleChange}
+            >
+              <option value="">Select Country</option>
+              <option value="China">China</option>
+              <option value="India">India</option>
+              <option value="United Kingdom">United Kingdom</option>
+              <option value="USA">USA</option>
+            </select>
+            <p role="alert">
+              {(touched.country || status === STATUS.SUBMITTED) && errors.country}
+            </p>
+          </div>
+
+          <div>
+            <input
+              type="submit"
+              className="btn btn-primary"
+              value="Save Shipping Info"
+              disabled={status === STATUS.SUBMITTING}
+            />
+          </div>
+        </form>
+      </>
+    )
   }
-  return (
-    <>
-      <h1>Shipping Info</h1>
-      {!isValid && status === STATUS.SUBMITTED && (
-        <div role="alert">
-          <p>Please Fix the following errors</p>
-          <ul>
-            {Object.keys(errors).map((key) => {
-              return <li key={key}>{errors[key]}</li>;
-            })}
-          </ul>
-        </div>
-      )}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="city">City</label>
-          <br />
-          <input
-            id="city"
-            type="text"
-            value={address.city}
-            onBlur={handleBlur}
-            onChange={handleChange}
-          />
-          <p role="alert">
-            {(touched.city || status === STATUS.SUBMITTED) && errors.city}
-          </p>
-        </div>
 
-        <div>
-          <label htmlFor="country">Country</label>
-          <br />
-          <select
-            id="country"
-            value={address.country}
-            onBlur={handleBlur}
-            onChange={handleChange}
-          >
-            <option value="">Select Country</option>
-            <option value="China">China</option>
-            <option value="India">India</option>
-            <option value="United Kingdom">United Kingdom</option>
-            <option value="USA">USA</option>
-          </select>
-          <p role="alert">
-            {(touched.country || status === STATUS.SUBMITTED) && errors.country}
-          </p>
-        </div>
-
-        <div>
-          <input
-            type="submit"
-            className="btn btn-primary"
-            value="Save Shipping Info"
-            disabled={status === STATUS.SUBMITTING}
-          />
-        </div>
-      </form>
-    </>
-  );
 }
