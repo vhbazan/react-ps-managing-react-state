7830a521b5080b3f0af445c6218ccd7269438cdf
Bazan Milla <victor_bazan_milla@optum.com>
2021-03-07 13:00:08 +0100

    Add DetailWraper and refactor Detail component to be a class component

--- src/Detail.class.jsx
+++ src/Detail.class.jsx
@@ -1,39 +1,58 @@
-import React, { useState } from "react";
+import React from "react";
 import { useParams, useNavigate } from 'react-router-dom';
 import useFetch from './useFetch';
 import Spinner from './Spinner';
 import PageNotFound from "./PageNotFound";
 import { useCart } from "./context/cartContext";
 
-export default function Detail() {
+export default function DetailWrapper() {
   const { dispatch } = useCart();
   const { id } = useParams();
-  const navigate = useNavigate();
-  const { data: product, error, loading } = useFetch("products/" + id);
-  const [sku, setSku] = useState("");
-  if (loading) return <Spinner />
-  if (!product) return <PageNotFound />
-  if (error) throw error;
+  const fetchResponse = useFetch("products/" + id);
   return (
-    <div id="detail">
-      <h1>{product.name}</h1>
-      <p>{product.description}</p>
-      <p id="price">${product.price}</p>
-      <select id="size" onChange={(e) => setSku(e.target.value)}
-        value={sku}>
-        <option value="">What size?</option>
-        {product.skus.map(sku => (
-          <option value={sku.sku} key={sku.sku}>{sku.size}</option>
-        ))}
-      </select>
-      <p>
-        <button className="btn btn-primary" disabled={!sku}
-          onClick={() => {
-            dispatch({ type: 'ADD', id: product.id, sku });
-            navigate("/cart")
-          }}>Add to Cart</button>
-      </p>
-      <img src={`/images/${product.image}`} alt={product.category} />
-    </div>
-  );
+    <Detail
+      id={id}
+      fetchResponse={fetchResponse}
+      navigate={useNavigate()}
+      dispatch={dispatch}
+    />
+  )
 }
+
+class Detail extends React.Component {
+  state = {
+    sku: ''
+  }
+  render() {
+    const { id, fetchResponse, navigate, dispatch } = this.props;
+    const { data: product, loading, error } = fetchResponse;
+    const { sku } = this.state;
+    if (loading) return <Spinner />
+    if (!product) return <PageNotFound />
+    if (error) throw error;
+    return (
+      <div id="detail">
+        <h1>{product.name}</h1>
+        <p>{product.description}</p>
+        <p id="price">${product.price}</p>
+        <select id="size" onChange={(e) => this.setState({ sku: e.target.value })}
+          value={sku}>
+          <option value="">What size?</option>
+          {product.skus.map(sku => (
+            <option value={sku.sku} key={sku.sku}>{sku.size}</option>
+          ))}
+        </select>
+        <p>
+          <button className="btn btn-primary" disabled={!sku}
+            onClick={() => {
+              dispatch({ type: 'ADD', id: product.id, sku });
+              navigate("/cart")
+            }}>Add to Cart</button>
+        </p>
+        <img src={`/images/${product.image}`} alt={product.category} />
+      </div>
+    );
+  }
+}
+
+
