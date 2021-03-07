71e84d546033f29b017d4e41e0048a6741da8965
Bazan Milla <victor_bazan_milla@optum.com>
2021-03-07 13:13:23 +0100

    Use Render prop pattern in Detail.class

--- src/Detail.class.jsx
+++ src/Detail.class.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams, useNavigate } from 'react-router-dom';
-import useFetch from './useFetch';
+import { Fetch } from './useFetch';
 import Spinner from './Spinner';
 import PageNotFound from "./PageNotFound";
 import { useCart } from "./context/cartContext";
@@ -8,11 +8,9 @@ import { useCart } from "./context/cartContext";
 export default function DetailWrapper() {
   const { dispatch } = useCart();
   const { id } = useParams();
-  const fetchResponse = useFetch("products/" + id);
   return (
     <Detail
       id={id}
-      fetchResponse={fetchResponse}
       navigate={useNavigate()}
       dispatch={dispatch}
     />
@@ -24,33 +22,39 @@ class Detail extends React.Component {
     sku: ''
   }
   render() {
-    const { id, fetchResponse, navigate, dispatch } = this.props;
-    const { data: product, loading, error } = fetchResponse;
+    const { id, navigate, dispatch } = this.props;
     const { sku } = this.state;
-    if (loading) return <Spinner />
-    if (!product) return <PageNotFound />
-    if (error) throw error;
     return (
-      <div id="detail">
-        <h1>{product.name}</h1>
-        <p>{product.description}</p>
-        <p id="price">${product.price}</p>
-        <select id="size" onChange={(e) => this.setState({ sku: e.target.value })}
-          value={sku}>
-          <option value="">What size?</option>
-          {product.skus.map(sku => (
-            <option value={sku.sku} key={sku.sku}>{sku.size}</option>
-          ))}
-        </select>
-        <p>
-          <button className="btn btn-primary" disabled={!sku}
-            onClick={() => {
-              dispatch({ type: 'ADD', id: product.id, sku });
-              navigate("/cart")
-            }}>Add to Cart</button>
-        </p>
-        <img src={`/images/${product.image}`} alt={product.category} />
-      </div>
+      <Fetch url={`products/${id}`} render={
+        (product, loading, error) => {
+          if (loading) return <Spinner />
+          if (!product) return <PageNotFound />
+          if (error) throw error;
+          return (
+            <div id="detail">
+              <h1>{product.name}</h1>
+              <p>{product.description}</p>
+              <p id="price">${product.price}</p>
+              <select id="size" onChange={(e) => this.setState({ sku: e.target.value })}
+                value={sku}>
+                <option value="">What size?</option>
+                {product.skus.map(sku => (
+                  <option value={sku.sku} key={sku.sku}>{sku.size}</option>
+                ))}
+              </select>
+              <p>
+                <button className="btn btn-primary" disabled={!sku}
+                  onClick={() => {
+                    dispatch({ type: 'ADD', id: product.id, sku });
+                    navigate("/cart")
+                  }}>Add to Cart</button>
+              </p>
+              <img src={`/images/${product.image}`} alt={product.category} />
+            </div>
+          )
+        }
+      }
+      />
     );
   }
 }
